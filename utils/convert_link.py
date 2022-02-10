b4eb5ab30e5b93d9992705182d42061432cffb0b
Kyohei Horikawa <kyohei3430@gmail.com>
2022-02-11 04:27:02 +0900

    fix conver_link.py

--- utils/convert_link.py
+++ utils/convert_link.py
@@ -11,10 +11,12 @@ for file in files:
     with open(file, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            res = re.match(r"!\[\]\((.*)\)", line)
+            res = re.match(r"!\[(.*)\]\((.*)\)", line)
             if res:
-                img_path = res.groups()[0]
-                new_line = f"![]({dirpath}{img_path})\n"
-                lines[lines.index(line)] = new_line
+                alt = res.groups()[0]
+                img_path = res.groups()[1]
+                if not img_path[0] == '/':
+                    new_line = f"![{alt}]({dirpath}{img_path})\n"
+                    lines[lines.index(line)] = new_line
     with open(file, 'w') as f:
         f.writelines(lines)
