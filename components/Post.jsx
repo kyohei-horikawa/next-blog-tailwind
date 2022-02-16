211ab2f62f265f4487a09eb16b3b76c9dd21339f
Kyohei Horikawa <kyohei3430@gmail.com>
2022-02-16 20:34:17 +0900

    add tag7

--- components/Post.jsx
+++ components/Post.jsx
@@ -4,29 +4,26 @@ import Image from "next/image";
 
 export const Post = ({ post }) => {
   const { title, date, tags, slug } = post;
-  console.log(post);
   return (
     <div
       key={post.title}
       className="flex items-center border-t-4 border-gray-400 dark:border-gray-100 pt-4 my-4"
     >
-      <div>
+      <div className="w-[100px] sm:w-[250px]">
         <Image
           src={`/posts/${slug}/cover.png`}
           width={250}
           height={150}
-          layout="fixed"
+          layout="responsive"
           priority={true}
         />
       </div>
-      <div className="pl-8">
+      <div className="pl-8 w-[200px]">
         <Link href={`/posts/${slug}`}>
           <a className="font-bold text-4xl ml-8">{title}</a>
         </Link>
         <p className="ml-8 mt-2">{date}</p>
-        <div className="flex">
-          <Tags tags={tags} title={title} />
-        </div>
+        <Tags tags={tags} title={title} />
       </div>
     </div>
   );
