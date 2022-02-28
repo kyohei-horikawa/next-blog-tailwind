a629fb7070ec02ec869653e6b3bde3d8627841bf
Kyohei Horikawa <kyohei3430@gmail.com>
2022-02-28 22:24:28 +0900

    fix main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,50 @@
-import Link from "next/link";
+import Head from "next/head";
 
-export default function Home() {
+import React from "react";
+
+import { getPostBySlug } from "utils/getPostBySlug";
+import { markdownToHtml } from "utils/markdownToHtml";
+
+import { unified } from "unified";
+import rehypeParse from "rehype-parse";
+import rehypeReact from "rehype-react";
+
+import { Title } from "components/Title";
+
+export const getStaticProps = async () => {
+  const post = getPostBySlug("/このブログについて");
+  const content = await markdownToHtml(post.content);
+  return { props: { post: { ...post, content } } };
+};
+
+const processor = unified()
+  .use(rehypeParse, { fragment: true })
+  .use(rehypeReact, {
+    createElement: React.createElement,
+  });
+
+const PostPage = ({ post }) => {
   return (
     <div>
-      <Link href="/posts">
-        <a>投稿一覧</a>
-      </Link>
+      <Head>
+        <title>{post.title}</title>
+        <meta property="description" content={post.summary} />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.summary} />
+        <meta property="og:image" content={`/posts/${post.slug}/cover.png`} />
+        <meta name="twitter:card" content={`/posts/${post.slug}/cover.png`} />
+      </Head>
+      <Title
+        title={post.title}
+        date={post.date}
+        tags={post.tags}
+        slug={post.slug}
+      />
+      <section className="relative">
+        {processor.processSync(post.content).result}
+      </section>
     </div>
   );
-}
+};
+
+export default PostPage;
